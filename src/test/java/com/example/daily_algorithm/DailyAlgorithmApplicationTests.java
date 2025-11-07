a672a2c64c24f95ba38d99931c3d290338642748
김민식 <u1r2r3@naver.com>
2025-11-07 16:10:57 +0900

    first commit

---
+++ src/test/java/com/example/daily_algorithm/DailyAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daily_algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
