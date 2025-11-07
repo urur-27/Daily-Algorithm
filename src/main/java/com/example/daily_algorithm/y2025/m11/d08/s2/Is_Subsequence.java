1cb410e0a457b9c31d5a731858ae257cb682b3a1
김민식 <u1r2r3@naver.com>
2025-11-08 02:47:58 +0900

    2025-11-08
    Is Subsequence : 부분 수열 찾기

---
+++ src/main/java/com/example/daily_algorithm/y2025/m11/d08/s2/Is_Subsequence.java
@@ -0,0 +1,17 @@
+package com.example.daily_algorithm.y2025.m11.d08.s2;
+
+public class Is_Subsequence {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+}
