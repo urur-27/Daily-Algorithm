d3330ef2653c8f05b35a1c12574c47e2fb6b999a
김민식 <u1r2r3@naver.com>
2025-11-20 23:59:25 +0900

    2025-11-20
    Linked_List_Cycle_2

---
+++ src/main/java/com/example/daily_algorithm/y2025/m11/d20/Linked_List_Cycle_2.java
@@ -0,0 +1,39 @@
+package com.example.daily_algorithm.y2025.m11.d20;
+
+public class Linked_List_Cycle_2 {
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                ListNode ptr1 = head;
+                ListNode ptr2 = slow;
+
+                while (ptr1 != ptr2) {
+                    ptr1 = ptr1.next;
+                    ptr2 = ptr2.next;
+                }
+                return ptr1;
+            }
+        }
+
+        return null;
+
+    }
+
+}
