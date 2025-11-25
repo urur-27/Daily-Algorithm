26df5d729ef29ae84443ee5ea50c5ae402eaf760
김민식 <u1r2r3@naver.com>
2025-11-25 14:45:52 +0900

    2025-11-25
    Remove_Duplicates_from_Sorted_List

---
+++ src/main/java/com/example/daily_algorithm/y2025/m11/d25/Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,19 @@
+package com.example.daily_algorithm.y2025.m11.d25;
+
+public class Remove_Duplicates_from_Sorted_List {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+}
