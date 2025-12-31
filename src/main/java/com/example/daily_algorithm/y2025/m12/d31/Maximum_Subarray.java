d2b7a89ee790a7d5fcad79adb27e6e388fd53cbc
김민식 <u1r2r3@naver.com>
2025-12-31 22:51:23 +0900

    2025-12-31 Maximum Subarray

---
+++ src/main/java/com/example/daily_algorithm/y2025/m12/d31/Maximum_Subarray.java
@@ -0,0 +1,15 @@
+package com.example.daily_algorithm.y2025.m12.d31;
+
+public class Maximum_Subarray {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++){
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
